1c245cde05f02ccfeab653de0f1c0c679fb7abd3
Denis Vasilev <vassdeniss@gmail.com>
2025-03-03 18:10:54 +0200

    Initial commit

---
+++ backend/src/main/java/denis/f108349/hospital/HospitalApplication.java
@@ -0,0 +1,13 @@
+package denis.f108349.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/denis/f108349/hospital/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package denis.f108349.hospital;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
