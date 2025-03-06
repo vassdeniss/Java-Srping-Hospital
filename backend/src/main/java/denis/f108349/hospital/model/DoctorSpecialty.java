04b5bb0bcbee913a50bdb759470ede2391230c7f
Denis Vasilev <vassdeniss@gmail.com>
2025-03-06 17:05:36 +0200

    feat: added join table model

---
+++ backend/src/main/java/denis/f108349/hospital/model/DoctorSpecialty.java
@@ -0,0 +1,13 @@
+package denis.f108349.hospital.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@AllArgsConstructor
+@Table("DoctorSpecialty")
+public class DoctorSpecialty {
+    private String doctorId;
+    private String specialtyId;
+}
