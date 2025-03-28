d4a9ae5b7427217fb98121c8fa357e5a1f36c7d2
Denis Vasilev <vassdeniss@gmail.com>
2025-03-28 23:23:02 +0200

    feat: open api docs

--- backend/src/main/java/denis/f108349/hospital/HospitalApplication.java
+++ backend/src/main/java/denis/f108349/hospital/HospitalApplication.java
@@ -1,9 +1,11 @@
 package denis.f108349.hospital;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class HospitalApplication {
 
 	public static void main(String[] args) {
